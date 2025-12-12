4afcde5828e4b73a52a4671e7f9a09dc29103475
Lucien Lasseau <lucien@windbornesystems.com>
2025-12-11 19:17:47 -0800

    fix test

--- spec/dd_metadata_spec.rb
+++ spec/dd_metadata_spec.rb
@@ -8,9 +8,9 @@ describe 'cdds' do
   it 'prints created_at to stdout' do
     init_time = latest_init_time_for('wm4')
     output = run('dd_metadata', init_time, '--model', 'wm4')
-    # Expect at least one region header and a date line with a value
+    # Expect one line looking like "Calculated at 2025-12-11T06:24:28.308Z"
     expect(output.split("\n").size).to be == 1
-    expect(output).to match(/Created at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/)
+    expect(output).to match(/Calculated at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/)
     expect(output.downcase).not_to include('traceback')
   end
 end
